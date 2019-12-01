80dada1ac61f6cd97d0447bf13404c60a7ac63cc
Mathias Hammer <>
2019-12-01 16:54:17 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="container">
-    <v-ons-page>
-      <v-ons-toolbar>
-        <div class="center">Title</div>
-      </v-ons-toolbar>
+    <no-ssr>
+      <v-ons-page>
+        <v-ons-toolbar>
+          <div class="center">Title</div>
+        </v-ons-toolbar>
 
-      <p style="text-align: center">
-        <v-ons-button @click="$ons.notification.alert('Hello World!')">Click me!</v-ons-button>
-      </p>
-    </v-ons-page>
+        <p style="text-align: center">
+          <v-ons-button @click="$ons.notification.alert('Hello World!')">Click me!</v-ons-button>
+        </p>
+      </v-ons-page>
+    </no-ssr>
   </div>
 </template>
 
