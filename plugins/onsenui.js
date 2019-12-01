dd95e0722febb8b2b51d592be27bfce65e9b0d52
Mathias Hammer <>
2019-12-01 15:38:37 +0100

    Nuxt and onsenui install

---
+++ plugins/onsenui.js
@@ -0,0 +1,9 @@
+// Webpack CSS import
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
+// JS import
+import Vue from 'vue';
+import VueOnsen from 'vue-onsenui'; // This imports 'onsenui', so no need to import it separately
+
+Vue.use(VueOnsen); // VueOnsen set here as plugin to VUE. Done automatically if a call to window.Vue exists in the startup code.
