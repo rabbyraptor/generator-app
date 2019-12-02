f48a23fb863f8a78c8d07935c8b426bbcdca4e27
RoseAcademy <42844340+RoseAcademy@users.noreply.github.com>
2019-12-02 12:09:04 +0100

    Ad index page
    Opsætning af index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="container">
-    <p>{{ this.$route.path }}</p>
-    <p>hej</p>
+   <div class="indexbox">
+     <img class="indexlogo" src="../assets/img/generator_logo_uden_dato.png">
+     <h2>Festival</h2>
+     <h2>2020</h2>
+   </div>  
+  
+  <div class="buyticket">
+   <a id="a" href="#">
+   <h3>Buy Ticket</h3>
+   </a>
   </div>
+
+  <div class="spons">
+    <img class="logo" src="../assets/img/city_of_odense_albani.png">
+  </div> 
+
+
+   </div>
+
 </template>
 
 <script>
@@ -15,10 +31,6 @@ export default {
 .container {
   margin: 0 auto;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
 
 .title {
@@ -42,4 +54,43 @@ export default {
 .links {
   padding-top: 15px;
 }
+
+.indexbox {
+  width: 300px; 
+  height: 200px;
+  padding: 30px; 
+  margin: 70px auto;
+  background-color: whitesmoke;
+  text-align: center;
+}
+
+.indexlogo {
+  width: 75%;
+}
+
+.buyticket {
+  width: 200px; 
+  height: 50px;
+  padding: 10px; 
+  margin: 30px auto;
+  background-color: whitesmoke;
+  text-align: center; 
+}
+
+.spons {
+  width: 200px; 
+  height: 50px;
+   
+  margin: 90px auto;
+  text-align: center;
+}
+
+#a {
+  text-decoration: none;
+}
+
+.logo {
+  width: 90%; 
+}
+
 </style>
