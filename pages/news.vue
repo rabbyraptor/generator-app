8cd70c9e2aa22e65a2892929f75ade0e40a15516
Mathias Hammer <>
2019-12-02 12:05:50 +0100

    Add news page

--- pages/news.vue
+++ pages/news.vue
@@ -1,44 +1,89 @@
 <template>
-  <div class="container">
-    <p>{{ this.$route.path }}</p>
+  <div>
+    <h1 style="text-align:center;">News</h1>
+    <v-ons-list>
+      <v-ons-list-item v-for="news in news">
+        <div class="news-grid">
+          <div class="news-image" :style="{backgroundColor:news.backgroundColor}"></div>
+          <div class="news-content">
+            <b v-html="news.title" />
+            <p v-html="news.content" />
+          </div>
+          <div class="fav-heart">
+            <v-ons-icon icon="heart"></v-ons-icon>
+          </div>
+        </div>
+      </v-ons-list-item>
+    </v-ons-list>
   </div>
 </template>
 
 <script>
-
 export default {
+  data() {
+    return {
+      news: {
+        news1: {
+          title: "News 1",
+          content: "These are the number one news.",
+          backgroundColor: "blue"
+        },
+        news2: {
+          title: "News 2",
+          content: "These are the number two news.",
+          backgroundColor: "red"
+        },
+        news3: {
+          title: "News 3",
+          content: "These are the number three news.",
+          backgroundColor: "black"
+        },
+        news4: {
+          title: "News 4",
+          content: "These are the number four news.",
+          backgroundColor: "black"
+        },
+        news5: {
+          title: "News 5",
+          content: "These are the number five news.",
+          backgroundColor: "black"
+        },
+        news6: {
+          title: "News 6",
+          content: "These are the number six news.",
+          backgroundColor: "black"
+        },
+        
+      }
+    };
+  }
 };
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
+.list-item,
+.list-item__center {
+  padding: 0;
+}
+
+.news-grid {
+  display: grid;
+  grid-template-rows: 100%;
+  grid-template-columns: 100px minmax(100px, 1fr) 30px;
   align-items: center;
-  text-align: center;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.news-image {
+  height: 100px;
+  width: 100px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.news-content {
+  font-size: 14px;
+  padding: 12px;
 }
 
-.links {
-  padding-top: 15px;
+.fav-heart {
+
 }
 </style>
