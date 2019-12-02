aca551c4af6e8703a72940b8a60e6300cab95b2f
Mathias Hammer <>
2019-12-02 13:09:18 +0100

    Add favorite icons

--- pages/news.vue
+++ pages/news.vue
@@ -2,15 +2,16 @@
   <div>
     <h1 style="text-align:center;">News</h1>
     <v-ons-list>
-      <v-ons-list-item v-for="news in news">
+      <v-ons-list-item v-for="news in news" :key="news.title">
         <div class="news-grid">
           <div class="news-image" :style="{backgroundColor:news.backgroundColor}"></div>
           <div class="news-content">
             <b v-html="news.title" />
             <p v-html="news.content" />
           </div>
-          <div class="fav-heart">
-            <v-ons-icon icon="heart"></v-ons-icon>
+          <div class="fav-heart" @click="news.favorite = !news.favorite">
+            <ios-heart-icon v-if="news.favorite" />
+            <ios-heart-empty-icon v-else />
           </div>
         </div>
       </v-ons-list-item>
@@ -26,34 +27,39 @@ export default {
         news1: {
           title: "News 1",
           content: "These are the number one news.",
+          favorite: false,
           backgroundColor: "blue"
         },
         news2: {
           title: "News 2",
           content: "These are the number two news.",
+          favorite: true,
           backgroundColor: "red"
         },
         news3: {
           title: "News 3",
           content: "These are the number three news.",
+          favorite: false,
           backgroundColor: "black"
         },
         news4: {
           title: "News 4",
           content: "These are the number four news.",
-          backgroundColor: "black"
+          favorite: false,
+          backgroundColor: "blue"
         },
         news5: {
           title: "News 5",
           content: "These are the number five news.",
-          backgroundColor: "black"
+          favorite: false,
+          backgroundColor: "red"
         },
         news6: {
           title: "News 6",
           content: "These are the number six news.",
+          favorite: false,
           backgroundColor: "black"
-        },
-        
+        }
       }
     };
   }
@@ -84,6 +90,6 @@ export default {
 }
 
 .fav-heart {
-
+  font-size: 20px;
 }
 </style>
