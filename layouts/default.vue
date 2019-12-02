5e4521fcffd93fff132934f68e0aefa989262330
Mathias Hammer <>
2019-12-02 18:06:29 +0100

    CSS Cleaunp

--- layouts/default.vue
+++ layouts/default.vue
@@ -160,14 +160,6 @@ export default {
 
 
 <style>
-/* LINKS */
-a.nuxt-link {
-  text-decoration: none;
-  color: inherit;
-  width: 100%;
-  height: 100%;
-  font-size:20px;
-}
 
 /* HAMBURGER MENU */
 .menuToggle {
