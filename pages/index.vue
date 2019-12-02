ace5a512d85c07e9def27c295c590c3ec8a95e17
Mathias Hammer <>
2019-12-02 16:50:40 +0100

    Update index.vue
    CSS Cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -29,41 +29,17 @@ export default {
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  width: 100%;
   background-image: url(../assets/img/background3.png);
   background-size: cover;
   background-position: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  padding: 70px 0;
 }
 
 .indexbox {
   width: 250px; 
   height: 200px;
   padding: 20px; 
-  margin: 70px auto;
+  margin: auto;
   background-color: white;
   text-align: center;
   opacity: 0.80;
