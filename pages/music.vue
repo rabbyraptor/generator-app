79cc4b3ed0beb5f5c853e4f97570df6ee013b9fc
Mathias Hammer <>
2019-12-03 15:55:38 +0100

    Changed tabbar + centralized artist data through props

--- pages/music.vue
+++ pages/music.vue
@@ -1,20 +1,22 @@
 <template>
-<div style="margin-bottom:49px; overflow:hidden; min-height:100vh;">
+  <div style="margin-bottom:44px; overflow:hidden; min-height:100vh;">
     <h1 class="title">Music</h1>
-      <program v-if="activeTab == 'program'"></program>
-      <lineup v-if="activeTab == 'lineup'"></lineup>
-      <playlists v-if="activeTab == 'playlists'"></playlists>
-    <v-ons-tabbar position="bottom">
-      <v-ons-tab
-        v-for="(tab, index) in tabs"
-        :label="tab.label"
-        :badge="tab.badge"
-        :icon="tab.icon"
-        :key="tab.key"
-        @click="activeTab = tab.key"
-        :active="isActiveTab(tab.key)"
-      ></v-ons-tab>
-    </v-ons-tabbar>
+    <program :artists="this.artists" v-if="activeTab == 'program'"></program>
+    <lineup v-if="activeTab == 'lineup'"></lineup>
+    <playlists v-if="activeTab == 'playlists'"></playlists>
+    <v-ons-bottom-toolbar class="music-toolbar">
+      <ul>
+        <li
+          v-for="(tab, index) in tabs"
+          :label="tab.label"
+          :badge="tab.badge"
+          :icon="tab.icon"
+          :key="tab.key"
+          @click="activeTab = tab.key"
+          :style="isActiveTab(tab.key)"
+        >{{ tab.label }}</li>
+      </ul>
+    </v-ons-bottom-toolbar>
   </div>
 </template>
 
@@ -27,9 +29,9 @@ export default {
   methods: {
     isActiveTab(key) {
       if (this.activeTab == key) {
-        return true;
+        return "color: " + this.brandColors.red.hex;
       }
-      return false;
+      return null;
     }
   },
   data() {
@@ -37,16 +39,72 @@ export default {
       activeTab: "program",
       tabs: [
         {
-          label: "Program",
-          key: "program"
-        },
-        {
           label: "Line-up",
           key: "lineup"
         },
         {
+          label: "Program",
+          key: "program"
+        },
+        {
           label: "Playlists",
           key: "playlists"
+        }
+      ],
+      artists: [
+        {
+          title: "Busy doin' nothing",
+          time: "17:00",
+          venue: "Main Stage",
+          favorite: false,
+          imageUrl: "busy_doing_nothing",
+          info:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus molestie nunc non blandit. Euismod lacinia at quis risus sed vulputate odio. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Placerat in egestas erat imperdiet sed euismod nisi."
+        },
+        {
+          title: "Vkation",
+          time: "18:30",
+          venue: "Main Stage",
+          favorite: true,
+          imageUrl: "vkation",
+          info:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus molestie nunc non blandit. Euismod lacinia at quis risus sed vulputate odio. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Placerat in egestas erat imperdiet sed euismod nisi."
+        },
+        {
+          title: "Hedegaard",
+          time: "19:30",
+          venue: "Main Stage",
+          favorite: false,
+          imageUrl: "hedegaard",
+          info:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus molestie nunc non blandit. Euismod lacinia at quis risus sed vulputate odio. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Placerat in egestas erat imperdiet sed euismod nisi."
+        },
+        {
+          title: "Nonsens",
+          time: "20:30",
+          venue: "Main Stage",
+          favorite: false,
+          imageUrl: "nonsens",
+          info:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus molestie nunc non blandit. Euismod lacinia at quis risus sed vulputate odio. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Placerat in egestas erat imperdiet sed euismod nisi."
+        },
+        {
+          title: "Emil Stabil",
+          time: "21:30",
+          venue: "Main Stage",
+          favorite: false,
+          imageUrl: "emil_stabil",
+          info:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus molestie nunc non blandit. Euismod lacinia at quis risus sed vulputate odio. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Placerat in egestas erat imperdiet sed euismod nisi."
+        },
+        {
+          title: "Faustix",
+          time: "22:00",
+          venue: "Main Stage",
+          favorite: false,
+          imageUrl: "faustix",
+          info:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus molestie nunc non blandit. Euismod lacinia at quis risus sed vulputate odio. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Placerat in egestas erat imperdiet sed euismod nisi."
         }
       ]
     };
@@ -55,17 +113,38 @@ export default {
     program,
     lineup,
     playlists
+  },
+  computed: {
+    brandColors() {
+      return this.$store.getters["brandColors/getColors"];
+    }
   }
 };
 </script>
 
 <style>
-.tabbar{
+.tabbar {
   z-index: 4;
   position: fixed;
 }
 
-.ons-tabbar{
-  z-index: 4!important;
+.ons-tabbar {
+  z-index: 4 !important;
+}
+
+.music-toolbar {
+  position: fixed;
+  z-index: 2147483648;
+}
+
+.music-toolbar ul {
+  text-align: center;
+  text-transform: uppercase;
+  width: 100%;
+  padding: 12px;
+  list-style: none;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 1fr 1fr !important;
 }
 </style>
