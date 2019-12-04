809ee7ea377789ab78822197d81f75f0cd71ffc3
Mathias Hammer <>
2019-12-04 11:41:29 +0100

    Add Tickets page

--- pages/tickets.vue
+++ pages/tickets.vue
@@ -1,15 +1,90 @@
 <template>
   <div>
-    <p>{{ this.$route.path }}</p>
+    <h1 class="title">Tickets</h1>
+    <v-ons-list>
+      <v-ons-list-item expandable :expanded.sync="isExpanded">
+        <div class="center" style="padding:12px;">
+          <h3 style="font-size:24px;">Student Tickets</h3>
+        </div>
+        <div class="expandable-content">
+          <v-ons-list-item v-for="ticket in studentTickets" :key="ticket.type">
+            <div class="ticket-grid">
+              <div class="ticket-image" style="background-color:red"></div>
+              <div class="ticket-content">
+                <p style="font-weight:bold;" v-html="ticket.type" />
+                <p v-html="ticket.price" />
+                <p v-html="ticket.access" />
+              </div>
+            </div>
+          </v-ons-list-item>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item>
+        <div class="center" style="padding:12px; line-height:1;">
+          <h3 style="font-size:24px;">Non-student tickets</h3>
+        </div>
+      </v-ons-list-item>
+    </v-ons-list>
+    <img class="odense-albani-logo" src="../assets/img/city_of_odense_albani.png" />
   </div>
 </template>
 
 <script>
-
 export default {
+  data() {
+    return {
+      studentTickets: [
+        {
+          type: "Ticket 1",
+          price: "DKK 150,-",
+          access: "Standard"
+        },
+        {
+          type: "Ticket 2",
+          price: "DKK 175,-",
+          access: "Standard"
+        },
+        {
+          type: "Ticket 3",
+          price: "DKK 250,-",
+          access: "VIP"
+        },
+        {
+          type: "Ticket 4",
+          price: "DKK 300,-",
+          access: "VIP"
+        }
+      ]
+    };
+  }
 };
 </script>
 
 <style>
+.ticket-grid {
+  display: grid;
+  width: 100%;
+  grid-template-columns: 100px minmax(100px, 1fr);
+  align-items: center;
+}
 
+.ticket-image {
+  height: 100px;
+  width: 100px;
+  background-size: cover;
+}
+
+.ticket-content {
+  padding: 0 12px;
+  font-size: 20px;
+}
+
+.list-item,
+.list-item__center {
+  padding: 0;
+}
+
+.list-item__expandable-content {
+  padding: 0;
+}
 </style>
