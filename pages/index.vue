4d559ceb5a336536ae4d8928d21fd9447300b8b2
RoseAcademy <42844340+RoseAcademy@users.noreply.github.com>
2019-12-04 09:25:53 +0100

    Update index.vue
    Opdatering

--- pages/index.vue
+++ pages/index.vue
@@ -29,7 +29,7 @@ export default {
 
 <style>
 .container {
-  background-image: url(../assets/img/background3.jpg);
+  background-image: url(../assets/img/background3.png);
   background-size: cover;
   background-position: center;
   padding: 70px 0;
@@ -37,12 +37,11 @@ export default {
 
 .indexbox {
   width: 250px; 
-  height: 200px;
-  padding: 20px; 
+  height: 180px;
+  padding: 10px; 
   margin: auto;
   background-color: white;
   text-align: center;
-  opacity: 0.80;
 }
 
 .indexlogo {
@@ -54,9 +53,14 @@ export default {
   height: 70px;
   padding: 10px; 
   margin: 30px auto;
-  background-color: white;
+  background-color: red;
   text-align: center;
-  opacity: 0.80; 
+}
+
+.buyticket:active {
+  background-color: #1a17ac;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
 }
 
 .spons {
@@ -73,15 +77,15 @@ export default {
 
 .logo {
   width: 90%; 
-  opacity: 0.80;
 }
 
 #h2index {
-  font-size: 50px; 
+  font-size: 40px; 
 }
 
 #h3index {
-  font-size: 35px; 
+  font-size: 40px;
+  color: white;  
 }
 
 </style>
