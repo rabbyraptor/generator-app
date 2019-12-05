809681e3a023eedc636158353cb64ecf9fa5999d
Mathias Hammer <>
2019-12-05 16:07:27 +0100

    Create favHeartButton.vue

---
+++ components/favHeartButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div @click="addArtistFavorite()">
+    <ios-heart-icon v-if="this.activeartist.favorite" />
+    <ios-heart-empty-icon v-else />
+  </div>
+</template>
+<script>
+export default {
+  props: [
+    'activeartist'
+  ],
+  methods: {
+    addArtistFavorite() {
+            console.log(this.activeartist);
+            
+            this.activeartist.favorite = !this.activeartist.favorite;
+            if (this.activeartist.favorite == true) {
+                this.$ons.notification.toast('+ ' + this.activeartist.title + ' was added to your favorites!', { timeout: 1000, animation: 'fall' })
+            }
+            else {
+                this.$ons.notification.toast('- ' + this.activeartist.title + ' was removed from your favorites!', { timeout: 1000, animation: 'fall' })
+            }
+        }
+  },
+}
+</script>
