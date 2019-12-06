1086f6ed24bdbe3c60ab31d30881ebcaff985af8
Mathias Hammer <>
2019-12-06 15:19:01 +0100

    Create hovedSponsor.vue

---
+++ components/hovedSponsor.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <img class="odense-albani-logo" src="/img/albani_cityodense_white.png" />
+  </div>
+</template>
+<style>
+/* Odense + Albani Logo */
+.odense-albani-logo{
+  width: 100%;
+  margin:32px 0;
+  padding:0 16px;
+}
+</style>
