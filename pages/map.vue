8ed3c496afaf8a2c8ce6af606669d60c30c10068
Mathias Hammer <>
2019-12-11 00:31:12 +0100

    Update map.vue
    Added under construction text

--- pages/map.vue
+++ pages/map.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="page-container" :style="pageBackground(this.$route.name)">
     <h1 class="title">Map</h1>
+    <p style="color:white; text-align:center;">Site is currently under construction.</p>
   </div>
 </template>
 
